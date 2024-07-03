17ac7f671be719b0db4ea91842e56479c83f3ea2
8ReTid8 <125667954+8ReTid8@users.noreply.github.com>
2024-07-03 21:29:59 +0700

    y

--- code_nitid_PYTHON/playground.py
+++ code_nitid_PYTHON/playground.py
@@ -1,10 +1,20 @@
+def countsort(arr):
+    m = max(arr)
+    count = [0]*(m+1)
+    for i in range(len(arr)):
+        count[arr[i]]+=1
+    for i in range(1,m+1):
+        count[i]+=count[i-1]
+    sortarr = [0]*len(arr)
+    for i in range(len(arr)):
+        count[arr[i]]-=1
+        sortarr[count[arr[i]]] = arr[i]
+    return sortarr
+
 n = int(input())
 arr = []
 for i in range(n):
     temp = int(input())
     arr.append(temp)
-min = arr[0]
-for i in range(1,n):
-    if(min>arr[i]):
-        min = arr[i] 
-print(min)
+output = countsort(arr)
+print(output)
